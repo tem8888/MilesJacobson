183129d9b99faa2d98528e2710adfb79e5df742b
tem <tem.8888@yandex.ru>
2021-02-09 04:05:35 +0300

    upd

--- commands/TWindow_team.js
+++ commands/TWindow_team.js
@@ -2,51 +2,54 @@ const Squad = require('../models/Squad')
 const User = require('../models/User')
 
 module.exports = {
-	name: 'team',
-	description: 'Show team players',
-	execute(message, args) {
-		
-	User.findOne({$or: [{userId: message.author.id}, {assistId: message.author.id}]}).then((user) => {
-		if (!user) return message.channel.send(`❌ Ошибка! Доступно только для участников сетевой.`)
-		
-		Squad.find({club: user.club}).then((playerList) => {
+  name: 'team',
+  description: 'Show team players',
+  execute(message, args) {
+    User.findOne({
+      $or: [{ userId: message.author.id }, { assistId: message.author.id }],
+    }).then((user) => {
+      if (!user)
+        return message.channel.send(
+          `❌ Ошибка! Доступно только для участников сетевой.`
+        )
 
-			let players = ''
-      let ids = ''
-      let prices = ''
-      for (player of playerList) {
-        players += `**${player.name}**\n`
-        ids += `${player.uid}\n`
-        prices += `**${player.price}**\n`
-      }
+      Squad.find({ club: user.club }).then((playerList) => {
+        let players = ''
+        let ids = ''
+        let values = ''
+        for (player of playerList) {
+          players += `**${player.name}**\n`
+          ids += `${player.uid}\n`
+          values += `**${player.value}**\n`
+        }
 
-			message.channel.send({embed:{
-				color: 3553599,
-				title: `_ _               🛡️ ${user.club} 🛡️`,
-				footer: {
-      		text: `Всего игроков: ${playerList.length}`
-				},
-        fields: [
-          {
-            name: `_ _ Игрок`,
-            value: `_ _\n_ _  ${players}`,
-            inline: true
+        message.channel.send({
+          embed: {
+            color: 3553599,
+            title: `_ _               🛡️ ${user.club} 🛡️`,
+            footer: {
+              text: `Всего игроков: ${playerList.length}`,
+            },
+            fields: [
+              {
+                name: `_ _ Игрок`,
+                value: `_ _\n_ _  ${players}`,
+                inline: true,
+              },
+              {
+                name: `_ _ ID`,
+                value: `_ _\n_ _ ${ids}`,
+                inline: true,
+              },
+              {
+                name: `Стоимость, £ млн   _ _`,
+                value: `_ _\n ${values}`,
+                inline: true,
+              },
+            ],
           },
-          {
-            name: `_ _ ID`,
-            value: `_ _\n_ _ ${ids}`,
-            inline: true
-          },
-          {
-            name: `Стоимость, £ млн   _ _`,
-            value: `_ _\n ${prices}`,
-            inline: true
-          },
-        ],
-      }})
-		})
-	})
-	
-	
-	}
+        })
+      })
+    })
+  },
 }
