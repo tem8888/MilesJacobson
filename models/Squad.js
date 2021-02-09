c1b55df51192b31dd4610401d0f8daf8dbb156e1
tem <tem.8888@yandex.ru>
2021-02-09 04:02:08 +0300

    upd

--- models/Squad.js
+++ models/Squad.js
@@ -1,11 +1,10 @@
 const { Schema, model } = require('mongoose')
 
 const schema = new Schema({
-  uid: {type: String, required: true},
-  name: {type: String},
-  price: {type: Number, default: 0},
-  mins: {type: Number, default: 0},
-  club: {type: String}
+  club: { type: String },
+  uid: { type: String, required: true },
+  name: { type: String },
+  value: { type: Number, default: 0 },
 })
 
 module.exports = model('Squad', schema)
