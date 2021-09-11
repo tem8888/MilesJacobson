fac70eab61dee2c7e1228cc0c7eb0051f78223ff
tem <tem.8888@yandex.ru>
2021-09-11 19:58:56 +0300

    upd

---
+++ commands/format.js
@@ -0,0 +1,25 @@
+const Transfer = require('../models/Transfer')
+
+module.exports = {
+  name: 'f',
+  description: 'Search player',
+  execute(message, args) {
+    const user = message.mentions.users.first() || message.author
+    let content = message.content.slice(3)
+
+    setTimeout(async function () {
+      await message.delete()
+    }, 1000)
+
+    message.channel.send({
+      embed: {
+        color: 3553599,
+        description: `${content}`,
+        author: {
+          name: `${user.username}`,
+          icon_url: `${user.displayAvatarURL()}`,
+        },
+      },
+    })
+  },
+}
--- commands/formatMessage.js
+++ commands/formatMessage.js
@@ -1,28 +1,30 @@
 module.exports = {
-	name: 'f',
-	description: 'Format message in embed',
-	execute(message, args) {
-		formatMessage = message.content.slice(2).split('|')
+  name: 'format',
+  description: 'Format message in embed',
+  execute(message, args) {
+    formatMessage = message.content.slice(2).split('|')
 
-		let msg = ' ▸ ' + formatMessage[1] + ' ◂ '
-		let title = ' ' + formatMessage[0] + ' '
+    let msg = ' ▸ ' + formatMessage[1] + ' ◂ '
+    let title = ' ' + formatMessage[0] + ' '
 
-		while (msg.length < 70) {
-			msg = [' ▸ ', msg, ' ◂ '].join('')
-		}
+    while (msg.length < 70) {
+      msg = [' ▸ ', msg, ' ◂ '].join('')
+    }
 
-    setTimeout(async function(){
-      await message.delete(); 
-    }, 2000);
-    message.channel.send({embed:{
-      color: 3553599,
-      title: `_ _${title}_ _\n`,
-      description: `_ _\n[${msg.toUpperCase()}](${formatMessage[2]})\n_ _\n${formatMessage[4].trim()}`,
-     	thumbnail: {
-     		url: `${formatMessage[3].trim()}`
-      }
-    }});
-		
-	}
+    setTimeout(async function () {
+      await message.delete()
+    }, 2000)
+    message.channel.send({
+      embed: {
+        color: 3553599,
+        title: `_ _${title}_ _\n`,
+        description: `_ _\n[${msg.toUpperCase()}](${
+          formatMessage[2]
+        })\n_ _\n${formatMessage[4].trim()}`,
+        thumbnail: {
+          url: `${formatMessage[3].trim()}`,
+        },
+      },
+    })
+  },
 }
- 
